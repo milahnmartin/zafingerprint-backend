a7946615655e93e490b5919ccfd276f50d84d4b3
Milahn <milahnmartin.develop@gmail.com>
2020-12-24 14:02:12 +0200

    init

--- za-fp-backend.js
+++ za-fp-backend.js
@@ -2,18 +2,67 @@ var express = require("express");
 var app = express();
 var port = 3001;
 
-var players = {
+var players_fortnite = {
   Travis: {
     mouse: 'polla',
     keyboard: 'kolla',
     age: 19,
-    gamertag: 'Trv'
+    gamertag: 'Trv',
+    biography: 'wkfjnwevå'
   },
   Milahn: {
     mouse: 'jolla',
     keyboard: 'lpppa',
     age: 20,
-    gamertag: 'Polla'
+    gamertag: 'Polla',
+    biography: 'wknjcwek'
+  },
+  Hutchie:{
+    mouse: 'razor',
+    keyboard: 'razor',
+    age: 32,
+    gamertag: 'Hutchie',
+    biography: 'wfweif'
+  }
+
+};
+
+var players_csgo = {
+  Travis: {
+    mouse: 'polla',
+    keyboard: 'kolla',
+    age: 19,
+    gamertag: 'Trv',
+    biography: 'wkfjnwevå'
+  },
+  Milahn: {
+    mouse: 'jolla',
+    keyboard: 'lpppa',
+    age: 20,
+    gamertag: 'Polla',
+    biography: 'wknjcwek'
+  },
+  Hutchie:{
+    mouse: 'razor',
+    keyboard: 'razor',
+    age: 32,
+    gamertag: 'Hutchie',
+    biography: 'wfweif'
+  },
+  Player:{
+    biography: '',
+    mouse: '',
+    age: null,
+    earnings: null,
+    signed:['Free-Agent','team-name'],
+    sens: ['dpi','ingame-sens','mouse-accel'],
+    monitor:  '',
+    resolution: '',
+    social_media: ['Twitter','Instagram','Youtube','etd'],
+    debut: ''
+
+
+
   }
 
 };
@@ -25,9 +74,14 @@ var players = {
 
 
 
-app.get("/data", (req, res) => {
-  res.send(players);
+app.get("/data/fortnite", (req, res) => {
+  res.send(players_fortnite);
 });
+
+app.get("/data/csgo", (req, res) => {
+  res.send(players_csgo);
+});
+
 
 
 
